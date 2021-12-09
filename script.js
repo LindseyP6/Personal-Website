410534604491fdfeec614ba79cee88da0d3c0b96
Lindsey Pregent <lindsey.pregent@gmail.com>
2021-12-09 13:59:46 -0500

    Created inital HTML, CSS, JS content

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 const h2 = document.createElement("h2");
-h2.textContent = "Favorite Things";
+h2.textContent = "Switching careers, Seeking freedom.";
 
 document.querySelector("body").appendChild(h2);
+
+let d = new Date();
+        alert("Hello Friend! Today's date is " + d);
+
+function myFunction() {
+    alert("More to come =)");
+    }
